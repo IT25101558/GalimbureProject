55e08b9f64fbab57f4499197d0364cf051ae4a2e
thevi <it25101558@my.sliit.lk>
2026-06-18 16:14:56 +0530

    Creating project files

---
+++ src/main/java/com/example/galimbureproject/GalimbureProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.galimbureproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalimbureProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalimbureProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/galimbureproject/GalimbureProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.galimbureproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalimbureProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
